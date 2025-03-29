f06ed0245d866ffc3c65d69bbd20c258c80b5a04
Arnav Kharbanda <arnav.std004@gmail.com>
2025-03-29 12:35:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Tawk.to Chat Widget Integration
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function() {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/YOUR_TAWK_TO_PROPERTY_ID/default'; // Replace with your Tawk.to Property ID
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+
+// Chat Button Click Event
+document.getElementById("chat-button").addEventListener("click", function() {
+    Tawk_API.toggle(); // Open/close chat window
+});
